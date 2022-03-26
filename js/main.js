5b65540cf3c4b40c3cea8d3e9bf8932c554f5870
sneakerfallich <shinkevich_01@mail.ru>
2022-03-26 23:59:30 +0600

    create: added images and Keyboard Control

--- js/main.js
+++ js/main.js
@@ -7,4 +7,9 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
 });
