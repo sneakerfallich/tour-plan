f11324544cf86ad216e212bf25c440ffacded140
sneakerfallich <shinkevich_01@mail.ru>
2022-04-25 02:29:56 +0300

    create: added form validate

--- js/main.js
+++ js/main.js
@@ -61,4 +61,23 @@ menuButton.on('click', function () {
       modalOverlay.removeClass('modal__overlay--visible');
       modalDialog.removeClass('modal__dialog--visible');
     }
+    //Обработка форм 
+    $('.form').each(function() {
+      $(this).validate({
+      errorClass: "invalid",
+      messages: {
+    name: {
+      required: "Укажите имя",
+      minlength: "Имя должно быть не короче 2 символов"
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+    phone: {
+      required: "Телефон обязателен"
+    },
+  }
+    });
+    })
 });
