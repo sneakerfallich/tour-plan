99483e99661678e5e66b75cfc28dff85da35d20e
sneakerfallich <shinkevich_01@mail.ru>
2022-04-24 22:26:07 +0300

    create: update modal window

--- js/main.js
+++ js/main.js
@@ -40,6 +40,13 @@ menuButton.on('click', function () {
   var closeModalButton = $('.modal__close')
   modalButton.on('click', openModal);
   closeModalButton.on('click', closeModal);
+  $(document).keydown(function(e) {
+    if(e.which === 27) 
+      var modalOverlay = $('.modal__overlay');
+      var modalDialog = $('.modal__dialog');
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+  });
     
     function openModal() {
       var modalOverlay = $('.modal__overlay');
